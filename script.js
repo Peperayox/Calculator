4ebcce0d175a85e03581742d2718082c08426b46
peperayox <samitier.tomas@gmail.com>
2021-12-27 21:37:43 -0300

    commit updated script

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-console.log('nigger');
-console.log('f');
-let sqtrr = 'ffff';
+const buttons = document.querySelectorAll('button');
+let down = '';
+let up = '';
+let ans = '';
+let var1 = '';
+let var2 = '';
+
+buttons.forEach((button) => {
+  button.addEventListener('click', onClick);
+});
+
+function onClick() {
+  switch (this.id) {
+    case 'clear':
+      _clear();
+      console.log('clearnigger');
+      break;
+    case 'delete':
+      _delete();
+      console.log('nignog delete');
+      break;
+    case 'x':
+    case '+':
+    case '-':
+    case '÷':
+      operator();
+      console.log('x nigger');
+      break;
+    case '=':
+      console.log('igual funciona');
+      break;
+    default:
+      down += this.id;
+  }
+}
+function _clear() {
+  down = '';
+}
+function _delete() {
+  if (down) {
+    down = down.substring(0, down.length - 1);
+  }
+}
+function operator() {
+  up = down;
+  down = '';
+}
+function mul() {
+  return var1 * var2;
+}
+function div() {
+  return Math.round((var1 / var2) * 100000) / 100000;
+}
+function sum() {
+  return var1 + var2;
+}
+function minus() {
+  return var1 - var2;
+}
